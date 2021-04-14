8db81763b412ecfd74ca2b1df0f0d39af43f53d5
Azhagu@97 <azhaguvelskvl@gmail.com>
2021-04-14 17:54:14 +0530

    Method 1,2,3

---
+++ src/test/java/org/method/Method123.java
@@ -0,0 +1,14 @@
+package org.method;
+
+public class Method123 {
+	public void method1() {
+        System.out.println("method 1");
+	}
+	public void method2() {
+        System.out.println("method 2");
+	}
+	public void method3() {
+        System.out.println("method 3");
+	}
+
+}
